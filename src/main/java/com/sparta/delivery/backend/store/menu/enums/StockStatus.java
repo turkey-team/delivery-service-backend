58f89eeb959e9a70abcc68e1e08759e8a64d6df1
jvn1o <junok0987@naver.com>
2025-09-30 01:18:11 +0900

    feat(entities): add order, storeMenu, orderMenu entity

--- src/main/java/com/sparta/delivery/backend/store/menu/enums/StockStatus.java
+++ src/main/java/com/sparta/delivery/backend/store/menu/enums/StockStatus.java
@@ -1,3 +1,7 @@
 package com.sparta.delivery.backend.store.menu.enums;
 
-public enum StockStatus { ON_SALE, SOLD_OUT }
+public enum StockStatus {
+	ON_SALE,    // 판매 중
+	LOW_STOCK,  // 재고 적음
+	SOLD_OUT    // 품절
+}
