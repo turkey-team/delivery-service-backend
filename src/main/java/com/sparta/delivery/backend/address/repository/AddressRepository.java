b7c9122dda4e5e368017871cdd2fa5661d0de47c
JIUNG YANG <yangjiung286@gmail.com>
2025-09-30 10:34:31 +0900

    fix(entity) : Change PK type from Long to UUID

--- src/main/java/com/sparta/delivery/backend/address/repository/AddressRepository.java
+++ src/main/java/com/sparta/delivery/backend/address/repository/AddressRepository.java
@@ -1,8 +1,10 @@
 package com.sparta.delivery.backend.address.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sparta.delivery.backend.address.entity.Address;
 
-public interface AddressRepository extends JpaRepository<Address, Long> {
+public interface AddressRepository extends JpaRepository<Address, UUID> {
 }
