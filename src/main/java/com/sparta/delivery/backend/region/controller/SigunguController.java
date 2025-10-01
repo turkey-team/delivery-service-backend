2dc8995285351175420b544f3c2a8b6d18b8a485
geon <leegeonhee0204@naver.com>
2025-10-01 20:55:59 +0900

    feat(region-sigungu): 시/군/구 수정 API 구현

--- src/main/java/com/sparta/delivery/backend/region/controller/SigunguController.java
+++ src/main/java/com/sparta/delivery/backend/region/controller/SigunguController.java
@@ -7,13 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sparta.delivery.backend.region.dto.ReqCreateSigunguDto;
+import com.sparta.delivery.backend.region.dto.ReqUpdateSigunguDto;
 import com.sparta.delivery.backend.region.dto.ResCreateSigunguDto;
 import com.sparta.delivery.backend.region.dto.ResReadSigunguDto;
+import com.sparta.delivery.backend.region.dto.ResUpdateSigunguDto;
 import com.sparta.delivery.backend.region.service.SigunguService;
 
 import lombok.RequiredArgsConstructor;
@@ -39,6 +42,15 @@ public class SigunguController {
 		List<ResReadSigunguDto> responseDtoList = sigunguService.getAllSigungu(sidoId);
 
 		return ResponseEntity.ok(responseDtoList);
+	}
+
+	@PutMapping("/sido/{sidoId}/sigungu/{sigunguId}")
+	public ResponseEntity<ResUpdateSigunguDto> updateSigungu(
+		@PathVariable UUID sidoId, @PathVariable UUID sigunguId, @RequestBody ReqUpdateSigunguDto requestDto
+	) {
+		ResUpdateSigunguDto responseDto = sigunguService.updateSigungu(sidoId, sigunguId, requestDto);
+
+		return ResponseEntity.ok(responseDto);
 	}
 
 }
