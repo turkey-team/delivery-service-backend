eb654c8b07dd079e78dec27476597021ffbef7bb
geon <leegeonhee0204@naver.com>
2025-10-01 18:08:07 +0900

    feat(region-sido): 시/도 삭제 API 구현

--- src/main/java/com/sparta/delivery/backend/region/controller/SidoController.java
+++ src/main/java/com/sparta/delivery/backend/region/controller/SidoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,13 @@ public class SidoController {
 		ResUpdateDto responseDto = sidoService.updateSido(sidoId, requestDto);
 
 		return ResponseEntity.ok(responseDto);
+	}
+
+	@DeleteMapping("/sido/{sidoId}")
+	public ResponseEntity<Void> deleteSido(@PathVariable UUID sidoId) {
+		sidoService.deleteSido(sidoId);
+
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/sparta/delivery/backend/region/service/SidoService.java
+++ src/main/java/com/sparta/delivery/backend/region/service/SidoService.java
@@ -78,4 +78,15 @@ public class SidoService {
 		return ResUpdateDto.from(sido);
 	}
 
+	// 시·도 삭제
+	public void deleteSido(UUID sidoId) {
+		Sido sido = sidoRepository.findById(sidoId).orElseThrow(() -> {
+			log.warn("시/도 지역 검색 실패");
+			return new EntityNotFoundException("존재하지 않는 시/도입니다.");
+		});
+
+		// 임시로 null을 넘김
+		sido.softDelete(null);
+	}
+
 }
