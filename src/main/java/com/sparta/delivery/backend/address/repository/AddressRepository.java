6d8b1b4e47394788237a2ad32aa4fc307d6d9d31
JIUNG YANG <yangjiung286@gmail.com>
2025-10-01 20:05:14 +0900

    feat(address): 자신의 주소 조회 기능 구현

--- src/main/java/com/sparta/delivery/backend/address/repository/AddressRepository.java
+++ src/main/java/com/sparta/delivery/backend/address/repository/AddressRepository.java
@@ -1,10 +1,15 @@
 package com.sparta.delivery.backend.address.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.sparta.delivery.backend.address.entity.Address;
 
 public interface AddressRepository extends JpaRepository<Address, UUID> {
+
+	@Query("select a from Address a join fetch a.customer c join c.user u where u.id = :userId")
+	List<Address> findAllByUserId(Long userId);
 }
