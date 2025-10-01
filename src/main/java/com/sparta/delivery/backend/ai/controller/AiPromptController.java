ca633f935854f8830f264dcc46e886389396e9c7
geon <leegeonhee0204@naver.com>
2025-10-01 12:53:30 +0900

    feat(ai): ai 프롬프트 목록 조회 api 구현

--- src/main/java/com/sparta/delivery/backend/ai/controller/AiPromptController.java
+++ src/main/java/com/sparta/delivery/backend/ai/controller/AiPromptController.java
@@ -1,13 +1,17 @@
 package com.sparta.delivery.backend.ai.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sparta.delivery.backend.ai.dto.AiPromptRequestDto;
-import com.sparta.delivery.backend.ai.dto.AiPromptResponseDto;
+import com.sparta.delivery.backend.ai.dto.AiPromptCreateRequestDto;
+import com.sparta.delivery.backend.ai.dto.AiPromptCreateResponseDto;
+import com.sparta.delivery.backend.ai.dto.AiPromptReadResponseDto;
 import com.sparta.delivery.backend.ai.service.AiPromptService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,10 +24,17 @@ public class AiPromptController {
 	private final AiPromptService aiPromptService;
 
 	@PostMapping
-	public ResponseEntity<AiPromptResponseDto> createAiPrompt(@RequestBody AiPromptRequestDto requestDto) {
-		AiPromptResponseDto responseDto = aiPromptService.createAiPrompt(requestDto);
+	public ResponseEntity<AiPromptCreateResponseDto> createAiPrompt(@RequestBody AiPromptCreateRequestDto requestDto) {
+		AiPromptCreateResponseDto responseDto = aiPromptService.createAiPrompt(requestDto);
 
 		return ResponseEntity.ok(responseDto);
+	}
+
+	@GetMapping
+	public ResponseEntity<Page<AiPromptReadResponseDto>> getAllAiPrompt(Pageable pageable) {
+		Page<AiPromptReadResponseDto> responseDtoList = aiPromptService.getAllAiPrompts(pageable);
+
+		return ResponseEntity.ok(responseDtoList);
 	}
 
 }
--- src/main/java/com/sparta/delivery/backend/ai/dto/AiPromptCreateRequestDto.java
+++ src/main/java/com/sparta/delivery/backend/ai/dto/AiPromptCreateRequestDto.java
@@ -1,14 +1,12 @@
 package com.sparta.delivery.backend.ai.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-public class AiPromptResponseDto {
+public class AiPromptCreateRequestDto {
 
-	private String resMessage;
+	private String reqMessage;
 
 }
