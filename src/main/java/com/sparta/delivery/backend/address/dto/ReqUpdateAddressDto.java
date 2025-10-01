93ecd4bdc4305c6b183ddb926901ad9a80f02379
JIUNG YANG <yangjiung286@gmail.com>
2025-10-01 21:11:13 +0900

    feat(address): 주소 업데이트 기능 구현

---
+++ src/main/java/com/sparta/delivery/backend/address/dto/ReqUpdateAddressDto.java
@@ -0,0 +1,14 @@
+package com.sparta.delivery.backend.address.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class ReqUpdateAddressDto {
+
+	@NotBlank(message = "법정동 코드는 필수 입력 값입니다.")
+	private String regionCode;
+
+	@NotBlank(message = "주소는 필수 입력 값입니다.")
+	private String address;
+}
