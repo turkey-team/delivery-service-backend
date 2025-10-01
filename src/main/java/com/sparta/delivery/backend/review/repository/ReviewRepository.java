8978ee60b7895d0c0a5d731a9bbe1b8d396f534f
brian110326 <brian110326@gmail.com>
2025-10-01 19:26:09 +0900

    fix(review-api): review 조건문 추가

--- src/main/java/com/sparta/delivery/backend/review/repository/ReviewRepository.java
+++ src/main/java/com/sparta/delivery/backend/review/repository/ReviewRepository.java
@@ -16,6 +16,7 @@ public interface ReviewRepository extends JpaRepository<Review, UUID>, ReviewRep
 	// 평균평점이 함께 들어가있는 Store List로 조회
 	@Query("select s.id as storeId, s.name as storeName, "
 		+ "avg(r.rate) as avgRate, count(r.id) as reviewCnt from Store s left join Review r on s.id = r.store.id "
+		+ "where r.deletedAt = null "
 		+ "group by s.id")
 	List<Void> findStoresWithAverageRating();
 
@@ -23,7 +24,7 @@ public interface ReviewRepository extends JpaRepository<Review, UUID>, ReviewRep
 	// 평균평점이 함께 들어가있는 Store 단건 조회
 	@Query("select s.id as storeId, s.name as storeName, "
 		+ "avg(r.rate) as avgRate, count(r.id) as reviewCnt from Store s left join Review r on s.id = r.store.id "
-		+ "where s.id = :storeId "
+		+ "where s.id = :storeId and r.deletedAt = null "
 		+ "group by s.id")
 	Optional<Void> findStoreWithAverageRating(@Param("storeId") UUID storeId);
 
