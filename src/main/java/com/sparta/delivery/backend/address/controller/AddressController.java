8d6602778f7cb4b438907aa3c395c7a357337990
JIUNG YANG <yangjiung286@gmail.com>
2025-10-01 21:11:13 +0900

    feat(address): 주소 업데이트 기능 구현

--- src/main/java/com/sparta/delivery/backend/address/controller/AddressController.java
+++ src/main/java/com/sparta/delivery/backend/address/controller/AddressController.java
@@ -1,17 +1,21 @@
 package com.sparta.delivery.backend.address.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sparta.delivery.backend.address.dto.ReqRegisterAddressDto;
+import com.sparta.delivery.backend.address.dto.ReqUpdateAddressDto;
 import com.sparta.delivery.backend.address.dto.ResAddressDto;
 import com.sparta.delivery.backend.address.service.AddressService;
 import com.sparta.delivery.backend.security.UserDetailsImpl;
@@ -38,6 +42,16 @@ public class AddressController {
 	public List<ResAddressDto> getMyAddresses(@AuthenticationPrincipal UserDetailsImpl user) {
 		return addressService.getMyAddresses(user);
 	}
+
+	@PutMapping("/{id}")
+	public ResAddressDto updateAddress(
+		@PathVariable UUID id,
+		@RequestBody ReqUpdateAddressDto requestDto,
+		@AuthenticationPrincipal UserDetailsImpl user
+	) {
+		return addressService.updateAddress(id, requestDto, user);
+	}
+
 
 
 }
--- src/main/java/com/sparta/delivery/backend/address/service/AddressService.java
+++ src/main/java/com/sparta/delivery/backend/address/service/AddressService.java
@@ -1,17 +1,20 @@
 package com.sparta.delivery.backend.address.service;
 
 import java.util.List;
+import java.util.UUID;
 
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sparta.delivery.backend.address.dto.ReqRegisterAddressDto;
+import com.sparta.delivery.backend.address.dto.ReqUpdateAddressDto;
 import com.sparta.delivery.backend.address.dto.ResAddressDto;
 import com.sparta.delivery.backend.address.entity.Address;
 import com.sparta.delivery.backend.address.repository.AddressRepository;
 import com.sparta.delivery.backend.customer.entity.Customer;
 import com.sparta.delivery.backend.customer.repository.CustomerRepository;
+import com.sparta.delivery.backend.global.excpetion.NotFoundException;
+import com.sparta.delivery.backend.global.excpetion.UnauthorizedException;
 import com.sparta.delivery.backend.region.entity.Dong;
 import com.sparta.delivery.backend.region.repository.DongRepository;
 import com.sparta.delivery.backend.security.UserDetailsImpl;
@@ -31,11 +34,11 @@ public class AddressService {
 	@Transactional
 	public void registerAddress(ReqRegisterAddressDto requestDto, UserDetailsImpl user) {
 		Dong dong = dongRepository.findByCode(requestDto.getRegionCode()).orElseThrow(
-			() -> new IllegalArgumentException("주소지를 찾을 수 없습니다.")
+			() -> new NotFoundException("주소지를 찾을 수 없습니다.")
 		);
 
 		Customer customer = customerRepository.findByUserId(user.getId()).orElseThrow(
-			() -> new IllegalArgumentException("해당 User를 찾을 수 없습니다.")
+			() -> new NotFoundException("해당 User를 찾을 수 없습니다.")
 		);
 
 		Address address = Address.builder()
@@ -50,5 +53,23 @@ public class AddressService {
 	public List<ResAddressDto> getMyAddresses(UserDetailsImpl user) {
 		List<Address> findAddresses = addressRepository.findAllByUserId(user.getId());
 		return findAddresses.stream().map(ResAddressDto::from).toList();
+	}
+
+	@Transactional
+	public ResAddressDto updateAddress(UUID id, ReqUpdateAddressDto requestDto, UserDetailsImpl user) {
+		Address address = addressRepository.findById(id).orElseThrow(
+			() -> new NotFoundException("요청한 리소스를 찾을 수 없습니다.")
+		);
+
+		if (!address.getCustomer().getUser().getId().equals(user.getId())) {
+			throw new UnauthorizedException("권한이 없습니다.");
+		}
+
+		Dong dong = dongRepository.findByCode(requestDto.getRegionCode()).orElseThrow(
+			() -> new NotFoundException("해당 주소지를 찾을 수 없습니다.")
+		);
+
+		address.update(dong, requestDto.getAddress());
+		return ResAddressDto.from(address);
 	}
 }
