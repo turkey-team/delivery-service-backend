4b3dda69ffb1f265867d81198d4126dcfaa33fe1
kimpompu <domathing@naver.com>
2025-10-01 15:13:51 +0900

    Feat/add Store-Category, StoreDetails, Store-Image Repository

---
+++ src/main/java/com/sparta/delivery/backend/store/repository/StoreDetailsRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.delivery.backend.store.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sparta.delivery.backend.store.entity.StoreDetails;
+
+public interface StoreDetailsRepository extends JpaRepository<StoreDetails, UUID> {
+}
