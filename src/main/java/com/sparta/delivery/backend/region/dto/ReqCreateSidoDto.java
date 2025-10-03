b5e8f8b6fa114c960f41dc911ef98d6defb96434
geon <leegeonhee0204@naver.com>
2025-10-04 00:00:42 +0900

    feat(region): RequestDto 유효성 검사 적용

--- src/main/java/com/sparta/delivery/backend/region/dto/ReqCreateSidoDto.java
+++ src/main/java/com/sparta/delivery/backend/region/dto/ReqCreateSidoDto.java
@@ -1,5 +1,7 @@
 package com.sparta.delivery.backend.region.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,8 +9,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ReqCreateSidoDto {
 
+	@NotBlank(message = "시/도 이름은 필수입니다.")
+	@Size(max = 50, message = "시/도 이름은 최대 50자까지 가능합니다.")
 	private String name;
 
+	@NotBlank(message = "시/도 코드는 필수입니다.")
+	@Size(max = 2, message = "시/도 코드는 최대 2자까지 가능합니다.")
 	private String code;
 
 }
--- src/main/java/com/sparta/delivery/backend/region/dto/ReqCreateSigunguDto.java
+++ src/main/java/com/sparta/delivery/backend/region/dto/ReqCreateSigunguDto.java
@@ -1,5 +1,7 @@
 package com.sparta.delivery.backend.region.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,8 +9,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ReqCreateSigunguDto {
 
+	@NotBlank(message = "시/군/구 이름은 필수입니다.")
+	@Size(max = 50, message = "시/군/구 이름은 최대 50자까지 가능합니다.")
 	private String name;
 
+	@NotBlank(message = "시/군/구 코드는 필수입니다.")
+	@Size(max = 3, message = "시/군/구 코드는 최대 3자까지 가능합니다.")
 	private String code;
 
 }
