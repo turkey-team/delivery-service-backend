e8c7a482368e3ad1021bf9877272465b7edefd8e
brian110326 <brian110326@gmail.com>
2025-10-04 15:47:44 +0900

    fix(review api):RequestMapping 주소 버전 명시

--- src/main/java/com/sparta/delivery/backend/review/controller/ReviewController.java
+++ src/main/java/com/sparta/delivery/backend/review/controller/ReviewController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sparta.delivery.backend.review.dto.ReqCreateReviewDto;
@@ -26,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/v1")
 public class ReviewController {
 
 	private final ReviewService reviewService;
