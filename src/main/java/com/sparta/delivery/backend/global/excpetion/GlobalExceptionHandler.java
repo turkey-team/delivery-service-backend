9d98ce59462d3427d6044945f288fc152abbf649
geon <leegeonhee0204@naver.com>
2025-10-15 19:05:48 +0900

    feat(slack): 예외가 발생했을 때 slack으로 메세지 전송하는 SlackService 구현

--- src/main/java/com/sparta/delivery/backend/global/excpetion/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/delivery/backend/global/excpetion/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.sparta.delivery.backend.global.excpetion;
 
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -9,12 +11,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
+import com.sparta.delivery.backend.global.infra.slack.SlackService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
+	private final Optional<SlackService> slackService;
+
 	@ExceptionHandler({UnauthorizedException.class})
-	public ResponseEntity<ApiException> handleException(UnauthorizedException ex) {
-		ApiException apiException = new ApiException(ex.getMessage(), HttpStatus.UNAUTHORIZED.value());
+	public ResponseEntity<ApiException> handleException(UnauthorizedException ex, HttpServletRequest request) {
+		String errorMessage = ex.getMessage();
+		slackService.ifPresent(service ->
+			service.sendMessage(request.getRequestURI(), ex.getClass().getSimpleName(), errorMessage)
+		);
+
+		ApiException apiException = new ApiException(errorMessage, HttpStatus.UNAUTHORIZED.value());
 		return new ResponseEntity<>(
 			apiException,
 			HttpStatus.UNAUTHORIZED
@@ -22,8 +37,13 @@ public class GlobalExceptionHandler {
 	}
 
 	@ExceptionHandler({IllegalArgumentException.class})
-	public ResponseEntity<ApiException> handleException(IllegalArgumentException ex) {
-		ApiException apiException = new ApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+	public ResponseEntity<ApiException> handleException(IllegalArgumentException ex, HttpServletRequest request) {
+		String errorMessage = ex.getMessage();
+		slackService.ifPresent(service ->
+			service.sendMessage(request.getRequestURI(), ex.getClass().getSimpleName(), errorMessage)
+		);
+
+		ApiException apiException = new ApiException(errorMessage, HttpStatus.BAD_REQUEST.value());
 		return new ResponseEntity<>(
 			apiException,
 			HttpStatus.BAD_REQUEST
@@ -31,8 +51,13 @@ public class GlobalExceptionHandler {
 	}
 
 	@ExceptionHandler({DuplicateUsernameException.class})
-	public ResponseEntity<ApiException> handleException(DuplicateUsernameException ex) {
-		ApiException apiException = new ApiException(ex.getMessage(), HttpStatus.CONFLICT.value());
+	public ResponseEntity<ApiException> handleException(DuplicateUsernameException ex, HttpServletRequest request) {
+		String errorMessage = ex.getMessage();
+		slackService.ifPresent(service ->
+			service.sendMessage(request.getRequestURI(), ex.getClass().getSimpleName(), errorMessage)
+		);
+
+		ApiException apiException = new ApiException(errorMessage, HttpStatus.CONFLICT.value());
 		return new ResponseEntity<>(
 			apiException,
 			HttpStatus.CONFLICT
@@ -40,8 +65,13 @@ public class GlobalExceptionHandler {
 	}
 
 	@ExceptionHandler({NotFoundException.class})
-	public ResponseEntity<ApiException> handleException(NotFoundException ex) {
-		ApiException apiException = new ApiException(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+	public ResponseEntity<ApiException> handleException(NotFoundException ex, HttpServletRequest request) {
+		String errorMessage = ex.getMessage();
+		slackService.ifPresent(service ->
+			service.sendMessage(request.getRequestURI(), ex.getClass().getSimpleName(), errorMessage)
+		);
+
+		ApiException apiException = new ApiException(errorMessage, HttpStatus.NOT_FOUND.value());
 		return new ResponseEntity<>(
 			apiException,
 			HttpStatus.NOT_FOUND
@@ -49,8 +79,14 @@ public class GlobalExceptionHandler {
 	}
 
 	@ExceptionHandler({HttpMessageNotReadableException.class})
-	public ResponseEntity<ApiException> handleException(HttpMessageNotReadableException ex) {
-		ApiException apiException = new ApiException("잘못된 JSON 형식입니다.", HttpStatus.BAD_REQUEST.value());
+	public ResponseEntity<ApiException> handleException(HttpMessageNotReadableException ex,
+		HttpServletRequest request) {
+		String errorMessage = "잘못된 JSON 형식입니다.";
+		slackService.ifPresent(service ->
+			service.sendMessage(request.getRequestURI(), ex.getClass().getSimpleName(), errorMessage)
+		);
+
+		ApiException apiException = new ApiException(errorMessage, HttpStatus.BAD_REQUEST.value());
 		return new ResponseEntity<>(
 			apiException,
 			HttpStatus.BAD_REQUEST
@@ -58,8 +94,13 @@ public class GlobalExceptionHandler {
 	}
 
 	@ExceptionHandler({MethodArgumentNotValidException.class})
-	public ResponseEntity<ApiException> handleException(MethodArgumentNotValidException ex) {
+	public ResponseEntity<ApiException> handleException(MethodArgumentNotValidException ex,
+		HttpServletRequest request) {
 		String errorMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+		slackService.ifPresent(service ->
+			service.sendMessage(request.getRequestURI(), ex.getClass().getSimpleName(), errorMessage)
+		);
+
 		ApiException apiException = new ApiException(errorMessage, HttpStatus.BAD_REQUEST.value());
 		return new ResponseEntity<>(
 			apiException,
@@ -68,8 +109,13 @@ public class GlobalExceptionHandler {
 	}
 
 	@ExceptionHandler({HandlerMethodValidationException.class})
-	public ResponseEntity<ApiException> handleException(HandlerMethodValidationException ex) {
+	public ResponseEntity<ApiException> handleException(HandlerMethodValidationException ex,
+		HttpServletRequest request) {
 		String errorMessage = ex.getAllErrors().get(0).getDefaultMessage();
+		slackService.ifPresent(service ->
+			service.sendMessage(request.getRequestURI(), ex.getClass().getSimpleName(), errorMessage)
+		);
+
 		ApiException apiException = new ApiException(errorMessage, HttpStatus.BAD_REQUEST.value());
 		return new ResponseEntity<>(
 			apiException,
@@ -78,8 +124,13 @@ public class GlobalExceptionHandler {
 	}
 
 	@ExceptionHandler({AccessDeniedException.class})
-	public ResponseEntity<ApiException> handleException(AccessDeniedException ex) {
-		ApiException apiException = new ApiException("권한이 없습니다.", HttpStatus.FORBIDDEN.value());
+	public ResponseEntity<ApiException> handleException(AccessDeniedException ex, HttpServletRequest request) {
+		String errorMessage = "권한이 없습니다.";
+		slackService.ifPresent(service ->
+			service.sendMessage(request.getRequestURI(), ex.getClass().getSimpleName(), errorMessage)
+		);
+
+		ApiException apiException = new ApiException(errorMessage, HttpStatus.FORBIDDEN.value());
 		return new ResponseEntity<>(
 			apiException,
 			HttpStatus.FORBIDDEN
---
+++ src/main/java/com/sparta/delivery/backend/global/infra/slack/SlackService.java
@@ -0,0 +1,66 @@
+package com.sparta.delivery.backend.global.infra.slack;
+
+import static com.slack.api.webhook.WebhookPayloads.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.slack.api.Slack;
+import com.slack.api.model.Attachment;
+import com.slack.api.model.Field;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@Profile("prod") // prod 환경에서만 활성화
+public class SlackService {
+
+	private final String webhook;
+	private final Slack slack;
+
+	public SlackService(@Value("${slack.api.webhook}") String webhook) {
+		this.webhook = webhook;
+		this.slack = Slack.getInstance();
+	}
+
+	public void sendMessage(String requestUrl, String exception, String message) {
+		try {
+			slack.send(webhook, payload(p -> p
+				.text(":rotating_light: *예외가 발생했습니다.*")
+				.attachments(List.of(
+					createAttachment(requestUrl, exception, message)
+				))
+			));
+		} catch (Exception e) {
+			log.warn("Slack API를 호출하지 못했습니다.");
+		}
+	}
+
+	private Attachment createAttachment(String requestUrl, String exception, String message) {
+		String requestTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now());
+		return Attachment.builder()
+			.color("#ff0000")
+			.fields(List.of(
+				createField("RequestTime", requestTime),
+				createField("RequestURL", requestUrl),
+				createField("Exception", exception),
+				createField("Message", message)
+			))
+			.build();
+	}
+
+	private Field createField(String title, String value) {
+		return Field.builder()
+			.title(title)
+			.value(value)
+			.valueShortEnough(false)
+			.build();
+	}
+
+}
