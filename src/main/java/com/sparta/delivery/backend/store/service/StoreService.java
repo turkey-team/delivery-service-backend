c44191483a96a7d13a06ba2a4045cf9824650a56
JIUNG YANG <yangjiung286@gmail.com>
2025-10-16 20:07:05 +0900

    feat(store): 가게 생성 시 배달 가능 지역 추가 기능 구현

--- src/main/java/com/sparta/delivery/backend/store/service/StoreService.java
+++ src/main/java/com/sparta/delivery/backend/store/service/StoreService.java
@@ -70,9 +70,7 @@ import com.sparta.delivery.backend.user.entity.UserRoleEnum;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class StoreService {
