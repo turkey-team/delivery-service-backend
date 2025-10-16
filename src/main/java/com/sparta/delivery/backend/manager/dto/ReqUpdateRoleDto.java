a94c142c876051b989fc466cdfeb8151e89b5683
geon <leegeonhee0204@naver.com>
2025-10-16 16:31:50 +0900

    fix(manager): ReqUpdateRoleDto에서 @NotBlank를 @NotNull로 변경

--- src/main/java/com/sparta/delivery/backend/manager/dto/ReqUpdateRoleDto.java
+++ src/main/java/com/sparta/delivery/backend/manager/dto/ReqUpdateRoleDto.java
@@ -2,7 +2,7 @@ package com.sparta.delivery.backend.manager.dto;
 
 import com.sparta.delivery.backend.user.entity.UserRoleEnum;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ReqUpdateRoleDto {
 
-	@NotBlank(message = "role은 필수입니다.")
+	@NotNull(message = "role은 필수입니다.")
 	private UserRoleEnum role;
 
 }
